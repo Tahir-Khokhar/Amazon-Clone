bd0fc168160b99b76a398dc63d8e0f06753bc43c
M.Tahir Nawaz <khokhartahir290@gmail.com>
2026-07-21 18:42:46 +0500

    Update admin.py

--- apps/addresses/admin.py
+++ apps/addresses/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Address
 
-# Register your models here.
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ['user', 'full_name', 'city', 'country', 'address_type', 'is_default', 'created_at']
+    list_filter = ['country', 'address_type', 'is_default', 'created_at']
+    search_fields = ['user__username', 'full_name', 'city', 'postal_code']
