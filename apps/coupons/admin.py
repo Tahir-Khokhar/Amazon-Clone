5bcd511cf092040b28e896379c7d34166098ccc5
M.Tahir Nawaz <khokhartahir290@gmail.com>
2026-07-22 06:46:23 +0500

    Update admin.py

--- apps/coupons/admin.py
+++ apps/coupons/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Coupon, CouponUsage
 
-# Register your models here.
+
+@admin.register(Coupon)
+class CouponAdmin(admin.ModelAdmin):
+    list_display = ['code', 'coupon_type', 'discount_value', 'is_active', 'valid_from', 'valid_until', 'used_count', 'usage_limit']
+    list_filter = ['coupon_type', 'is_active', 'valid_from', 'valid_until']
+    search_fields = ['code']
+    readonly_fields = ['used_count', 'created_at', 'updated_at']
+
+
+@admin.register(CouponUsage)
+class CouponUsageAdmin(admin.ModelAdmin):
+    list_display = ['coupon', 'user', 'order', 'used_at']
+    list_filter = ['used_at']
+    search_fields = ['coupon__code', 'user__username', 'order__order_number']
+    readonly_fields = ['used_at']
