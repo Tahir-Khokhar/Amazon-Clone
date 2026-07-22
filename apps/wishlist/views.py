2e9597419bcc2a1c94f1f3a257826bdbcb482053
Muhammad Tahir Nawaz <khokhartahir290@gmail.com>
2026-07-22 17:07:56 +0500

    Update views.py

--- apps/wishlist/views.py
+++ apps/wishlist/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.urls import path
+from .views import (
+    WishlistView,
+    WishlistAddView,
+    WishlistRemoveView,
+    WishlistMoveToCartView,
+)
 
-# Create your views here.
+app_name = 'wishlist'
+
+urlpatterns = [
+    path('', WishlistView.as_view(), name='wishlist'),
+    path('add/', WishlistAddView.as_view(), name='wishlist-add'),
+    path('<int:pk>/remove/', WishlistRemoveView.as_view(), name='wishlist-remove'),
+    path('<int:pk>/move-to-cart/', WishlistMoveToCartView.as_view(), name='wishlist-move-to-cart'),
+]
