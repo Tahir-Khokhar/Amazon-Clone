6cd5bb1d25dce690b627b7270b0ba3dd2279861a
Muhammad Tahir Nawaz <khokhartahir290@gmail.com>
2026-07-22 16:04:55 +0500

    Update admin.py

--- apps/dashboard/admin.py
+++ apps/dashboard/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import DashboardStat
 
-# Register your models here.
+
+@admin.register(DashboardStat)
+class DashboardStatAdmin(admin.ModelAdmin):
+    list_display = ['date', 'total_orders', 'total_revenue', 'total_users', 'total_products']
+    list_filter = ['date']
+    search_fields = ['date']
+    readonly_fields = ['date', 'total_orders', 'total_revenue', 'total_users', 'total_products', 'new_orders', 'new_users']
