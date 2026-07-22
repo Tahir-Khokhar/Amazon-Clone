33ae00777c2d460b1a3d4e150a3b36178541f873
Muhammad Tahir Nawaz <khokhartahir290@gmail.com>
2026-07-22 16:05:56 +0500

    Update views.py

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
 
-# Create your views here.
+from .models import DashboardStat
+from .serializers import AdminDashboardSerializer
+
+
+class AdminDashboardView(generics.ListAPIView):
+    serializer_class = AdminDashboardSerializer
+    permission_classes = [permissions.IsAdminUser]
+
+    def get_queryset(self):
+        return DashboardStat.objects.all()[:30]
+
+
+class SellerDashboardView(generics.ListAPIView):
+    serializer_class = AdminDashboardSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.role == "seller":
+            return DashboardStat.objects.all()[:30]
+        return DashboardStat.objects.none()
