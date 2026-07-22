3df9db2556967c5bc12072ceab5927009f6f9f03
M.Tahir Nawaz <khokhartahir290@gmail.com>
2026-07-22 06:50:20 +0500

    Update admin.py

--- apps/customers/admin.py
+++ apps/customers/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import CustomerProfile
 
-# Register your models here.
+
+@admin.register(CustomerProfile)
+class CustomerProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'total_orders', 'total_spent', 'loyalty_points', 'is_premium']
+    list_filter = ['is_premium']
+    search_fields = ['user__username', 'user__email']
+    readonly_fields = ['total_orders', 'total_spent', 'wishlist_count', 'loyalty_points']
