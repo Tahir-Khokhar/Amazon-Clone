094ca74b1be236deb318f535a531b6c1ac1e1930
M.Tahir Nawaz <khokhartahir290@gmail.com>
2026-07-22 06:50:32 +0500

    Update models.py

--- apps/customers/models.py
+++ apps/customers/models.py
@@ -1,3 +1,16 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+from apps.core.models import TimeStampedModel
+
+
+class CustomerProfile(TimeStampedModel):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='customer_profile')
+    total_orders = models.IntegerField(default=0)
+    total_spent = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+    wishlist_count = models.IntegerField(default=0)
+    loyalty_points = models.IntegerField(default=0)
+    is_premium = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user.username} - Customer Profile"
